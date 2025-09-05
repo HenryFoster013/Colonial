7f47d01d2b2c0a99a91623b6da911b5b955e7a97
HenryFoster013 <henryfoster013@gmail.com>
2025-09-05 01:13:27 +0100

    Added the lobby screen with the ability to change faction then start the game if there are two opposing factions. This has been a frustrating experience as the documentation on Photon Fusion is sparce and the way it works contradicts PUN2 which im used to. I am generally starting to question if I should've just stuck with the legacy PUN2 for this project. Nice to learn new things I suppose, and hey it works. The networking side can be a little slow but tbh thats totally 100% acceptable for a turn based strategy game. Glad I didn't go with the FPS idea first.

--- Assets/Scripts/PlayerInstance.cs
+++ Assets/Scripts/PlayerInstance.cs
@@ -3,27 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 
-public class PlayerInstance : MonoBehaviour{
+public class PlayerInstance : NetworkBehaviour{
 
-    [SerializeField] Faction _Faction;
+    [Networked] public int Faction_ID { get; set; }
+    [Networked, Capacity(32)] public string Username { get; set; }
+    [Networked] public bool Host { get; set; }
+    
+
     [SerializeField] FactionLookup _FactionLookup;
-
-    bool local_player;
     PlayerManager player_manager;
-    PlayerRef Owner;
+    private ChangeDetector _changeDetector;
+
+    public override void Spawned(){
+        if(Object.HasInputAuthority){
+            Username = PlayerPrefs.GetString("USERNAME");
+            Faction_ID = PlayerPrefs.GetInt("FACTION");
+            RPC_SetCoreData(PlayerPrefs.GetString("USERNAME"), PlayerPrefs.GetInt("FACTION"));
+        }
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void RPC_SetCoreData(string username, int faction_id, RpcInfo info = default){
+        Username = username;
+        Faction_ID = faction_id;
+    }
+
+    public void UpdateFaction(){
+        if(Object.HasInputAuthority){
+            //Faction_ID = PlayerPrefs.GetInt("FACTION");
+            RPC_SetCoreData(PlayerPrefs.GetString("USERNAME"), PlayerPrefs.GetInt("FACTION"));
+        }
+    }
 
     public void SnapCameraToPosition(Vector3 position){
-        if(local_player && player_manager != null){
+        if(Object.HasInputAuthority && player_manager != null){
             player_manager.SnapCameraToPosition(position);
         }
     }
 
     // Setters //
-    public void SetOwner(PlayerRef owner){Owner = owner;}
     public void SetManager(PlayerManager manager){player_manager = manager;}
-    public void SetLocal(bool local){local_player = local;}
+    public void WeAreHost(){Host = true;}
 
     // Getters //
-    public Faction FactionData(){return _Faction;}
-    public PlayerRef GetOwner(){return Owner;}
+    public Faction FactionData(){return _FactionLookup.GetFaction(Faction_ID);}
 }
