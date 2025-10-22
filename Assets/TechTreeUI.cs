1f4457045f2268bdeb2376a304c6718cf96ce361
Henry Foster <henryfoster013@gmail.com>
2025-10-22 01:23:29 +0100

    Created UI for tech tree, needs to be expanded

---
+++ Assets/TechTreeUI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TechTreeUI : MonoBehaviour
+{
+    // need to do all da math for making the forks correctly space out by themselves and nodes being placed correctly
+    // long one this, should be fun tho
+}
