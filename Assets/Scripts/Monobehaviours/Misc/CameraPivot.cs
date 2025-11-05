de13bf890ee7382a7681becfc126dbd461df88e9
Henry Foster <henryfoster013@gmail.com>
2025-11-05 23:18:33 +0000

    Updated the shaders, the bottom of tiles now blends into the background and so does the hexagonal grid. I also added a diorama to the title screen that can be spun with the mouse.

---
+++ Assets/Scripts/Monobehaviours/Misc/CameraPivot.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPivot : MonoBehaviour
+{
+    [SerializeField] Vector2 Sensitivity;
+    Vector2Int mouse_offset;
+
+    void Start(){
+        mouse_offset = new Vector2Int(Screen.width / 2, Screen.height / 2);
+    }
+
+    public void Update(){
+        print(Input.mousePosition.x + -mouse_offset.x);
+        float y = (Input.mousePosition.x - mouse_offset.x) * -Sensitivity.x;
+        float x = (Input.mousePosition.y - mouse_offset.y) * Sensitivity.y;
+        transform.eulerAngles = new Vector3(x, y, 0);
+    }
+}
