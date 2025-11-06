f267a654d4df0c2e56f099e7b2ddd3a94c2a6604
Henry Foster <henryfoster013@gmail.com>
2025-11-06 01:59:42 +0000

    Added a background music manager that dynamically fades music tracks and keeps them consistent between scenes

---
+++ Assets/Scripts/Monobehaviours/Misc/BG_MusicNode.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_MusicNode : MonoBehaviour
+{
+    [SerializeField] AudioSource _AudioSource;
+
+    float target_volume = 0;
+    float max_volume = -1;
+    bool mark_for_death = false;
+
+    void Start(){
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void Play(AudioClip clip){
+        if(clip == null)
+            Destroy(this.gameObject);
+        _AudioSource.clip = clip;
+        _AudioSource.Play();
+        target_volume = _AudioSource.volume;
+        _AudioSource.volume = 0;
+    }
+
+    void LateUpdate(){
+        if(_AudioSource.clip == null)
+            return;
+        _AudioSource.volume = Mathf.Lerp(_AudioSource.volume, target_volume, Time.deltaTime * 1.5f);
+        if(mark_for_death){
+            if(_AudioSource.volume < 0.01f){
+                Destroy(this.gameObject);
+            }
+        }
+    }
+
+    public void Terminate(){
+        target_volume = 0;
+        mark_for_death = true;
+    }
+
+    public bool Validate(AudioClip clip){return _AudioSource.clip == clip;}
+}
---
+++ Assets/Scripts/Monobehaviours/Misc/BG_MusicPlayer.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BG_MusicPlayer : MonoBehaviour
+{
+    [SerializeField] AudioClip Track;
+    [SerializeField] GameObject NodePrefab;
+
+    void Start(){
+        SearchForTrack();
+    }
+
+    public void SearchForTrack(){
+
+        GameObject[] all_nodes = GameObject.FindGameObjectsWithTag("Backing Track");
+        if(all_nodes.Length == 0){
+            CreateSong();
+            return;
+        }
+
+        bool song_exists = true;
+        foreach(GameObject g in all_nodes){
+            BG_MusicNode node = g.GetComponent<BG_MusicNode>();
+            if(!node.Validate(Track))
+                node.Terminate();
+            else
+                song_exists = true;
+        }
+
+        if(!song_exists)
+            CreateSong();
+    }
+
+    void CreateSong(){
+        if(Track == null)
+            return;
+        GameObject g = GameObject.Instantiate(NodePrefab);
+        g.GetComponent<BG_MusicNode>().Play(Track);
+    }
+}
